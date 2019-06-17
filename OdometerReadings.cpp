0a916cd2bcdb598b70dff4dd320f115e84214759
GSriyarsha <48006019+GSriyarsha@users.noreply.github.com>
2019-06-17 16:30:51 +0530

    Odometer reading in C++

---
+++ OdometerReadings.cpp
@@ -0,0 +1,50 @@
+#include <string.h>
+#include <bits/stdc++.h> 
+using namespace std; 
+public class OdometerReadings {
+	int initialiseValues(int len, String input){
+		int[] values = {12, 23, 34, 45, 56, 67, 78, 89};
+		return previousReadings(values, len, input);
+	}
+	int previousReadings(int[] values, int len, int input){
+		int result = 0;
+		int reading[] = {};
+		int digit = (int) (math.pow(10, len - 2));
+		int verify = (int) (math.pow(10, len - 1));
+		for(int i = 0; i < 7 - (len - 2); i ++){
+			if ((input % 10) != 0){
+				result = (values[i] * digit) + (input % 10) - 1;
+			}
+			else if ((math.floorDiv(input, verify) == 1) && (input % 10) != 0){
+				result = (values[7 - (len - 2)] * digit) + values[7 - (len - 2)] - 1;
+			}
+			reading[i] = result;
+			cout << readings[i];
+		}
+		return endReadings( values, len, input );
+	}
+	public static int endReadings(int[] values, int len, int input){
+		int result = 0;
+		int readings[] = {};
+		int digit = (int) (math.pow(10, len - 2));
+		int verify = (int) (math.pow(10, len - 1));
+		for (int i = 0; i < 7 - (len - 2); i ++){
+			if ((input % 10) != 0){
+				result = (values[i]*digit) + (input % 10) + 1;
+				readings[i] = result;
+			}
+			else if ((math.floorDiv(input, verify) == 9) && (input % 10) != 0){
+				result = (values[7 - (len - 2)] * digit) + values[7 - (len - 2)] + 1;
+				readings[i] = result; 
+			}
+			cout << readings[i];
+		}
+		return 1;
+		}
+
+	int main() {
+		String input = argsv[1];
+		int len = input.length();
+	  initialiseValues(len, input);
+	}
+}
